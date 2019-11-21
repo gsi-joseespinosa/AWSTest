b817f35d67e586bcd4e29c7125997a350f65c11c
gsi-joseespinosa <jose.espinosa@generalsoftwareinc.com>
2019-11-21 17:13:34 -0500

    New Pull Request

--- src/main/java/MessageUtil.java
+++ src/main/java/MessageUtil.java
@@ -4,6 +4,12 @@ public class MessageUtil {
 	 public MessageUtil(String message) {
 		this.message = message;
 	 }
+	  public MessageUtil2(String message) {
+		this.message = message;
+	 }
+	  public MessageUtil3(String message) {
+		this.message = message;
+	 }
 	 public String printMessage() {
 		 System.out.println(message);
 		 return message;
